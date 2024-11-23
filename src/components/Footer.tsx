e32bf511c58300ae944a0789d28af9ae8e91b968
panglimamhmmd <yahyakoto@gmail.com>
2024-11-23 14:52:57 +0700

    fix: footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -103,22 +103,7 @@ export function Footer() {
                 </div>
 
                 <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-                    Copyright © {new Date().getFullYear()}. Made with ♥ by{' '}
-                    <a
-                        href="https://web3templates.com/"
-                        target="_blank"
-                        rel="noopener"
-                    >
-                        Web3Templates.
-                    </a>{' '}
-                    Illustrations from{' '}
-                    <a
-                        href="https://www.glazestock.com/"
-                        target="_blank"
-                        rel="noopener "
-                    >
-                        Glazestock
-                    </a>
+                    Copyright © {new Date().getFullYear()}. Made with ♥
                 </div>
             </Container>
             {/* Do not remove this */}
